be2592a629083f4553165833037328872012127b
HyunSangWon <hyunsangwon93@gmail.com>
2020-05-29 23:48:09 +0900

    전화번호 예외처리

--- src/main/java/pro/cntech/inventory/service/MainService.java
+++ src/main/java/pro/cntech/inventory/service/MainService.java
@@ -67,9 +67,38 @@ public class MainService implements UserDetailsService
 
     public String setCompanyPhoneNumber(String phone)
     {
-        String firstNumber = phone.substring(0,3);
-        String secondNumber = phone.substring(3,6);
-        String lastNumber= phone.substring(6,10);
+        String firstNumber = null;
+        String secondNumber = null;
+        String lastNumber= null;
+
+        if(phone.length() == 11)
+        {
+            firstNumber = phone.substring(0,3);
+            secondNumber = phone.substring(3,7);
+            lastNumber= phone.substring(7,11);
+        }
+        if(phone.length() == 9)
+        {
+            firstNumber = phone.substring(0,2);
+            secondNumber = phone.substring(2,5);
+            lastNumber= phone.substring(5,9);
+        }
+        if(phone.length() == 8)
+        {
+            firstNumber = phone.substring(0,4);
+            secondNumber = phone.substring(4,8);
+            return firstNumber+"-"+secondNumber;
+        }
+        if(phone.length() == 10)
+        {
+            firstNumber = phone.substring(0,3);
+            secondNumber = phone.substring(3,6);
+            lastNumber= phone.substring(6,10);
+        }
+        else
+        {
+            return phone;
+        }
         return firstNumber+"-"+secondNumber+"-"+lastNumber;
     }
 
