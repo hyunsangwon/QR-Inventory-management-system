6330d8e1447e48801627fc9df0fed23b68fdb52b
HyunSangWon <hyunsangwon93@gmail.com>
2020-06-11 11:29:49 +0900

    거리 계산 class 생성

---
+++ src/main/java/pro/cntech/inventory/util/DistanceCalculation.java
@@ -0,0 +1,31 @@
+package pro.cntech.inventory.util;
+
+public class DistanceCalculation
+{
+
+    public double distance(double gpsLat, double gpsLon, double companyLat, double companyLon, String unit)
+    {
+        double theta = gpsLon - companyLon;
+        double dist = Math.sin(deg2rad(gpsLat)) * Math.sin(deg2rad(companyLat)) + Math.cos(deg2rad(gpsLat)) * Math.cos(deg2rad(companyLat)) * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+
+        if (unit == "kilometer") {
+            dist = dist * 1.609344;
+        } else if(unit == "meter"){
+            dist = dist * 1609.344;
+        }
+        return Math.round(dist);
+    }
+
+    public double deg2rad(double deg)
+    {
+        return (deg * Math.PI / 180.0);
+    }
+
+    public double rad2deg(double rad)
+    {
+        return (rad * 180 / Math.PI);
+    }
+}
--- src/test/java/pro/cntech/inventory/InventoryApplicationTests.java
+++ src/test/java/pro/cntech/inventory/InventoryApplicationTests.java
@@ -9,17 +9,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
-class InventoryApplicationTests {
-
+class InventoryApplicationTests
+{
+  	//http://www.geodatasource.com/developers/ 참고
 	@Test
 	void contextLoads()
 	{
-		List<String> list = new ArrayList<>();
-		list.add("hello");
-		list.add("bye");
-
-
-
+		// 킬로미터(Kilo Meter) 단위
+		int distanceKiloMeter =
+				(int)distance(36.4324528, 127.3944195, 36.343869459442, 127.448909230153, "meter");
+		System.out.println("distance ====> "+distanceKiloMeter);
  	}
 
+ 	public double distance(double gpsLat, double gpsLon, double companyLat, double companyLon, String unit)
+	{
+		double theta = gpsLon - companyLon;
+		double dist = Math.sin(deg2rad(gpsLat)) * Math.sin(deg2rad(companyLat)) + Math.cos(deg2rad(gpsLat)) * Math.cos(deg2rad(companyLat)) * Math.cos(deg2rad(theta));
+		dist = Math.acos(dist);
+		dist = rad2deg(dist);
+		dist = dist * 60 * 1.1515;
+
+		if (unit == "kilometer") {
+			dist = dist * 1.609344;
+		} else if(unit == "meter"){
+			dist = dist * 1609.344;
+		}
+		return Math.round(dist);
+	}
+
+	public double deg2rad(double deg)
+	{
+		return (deg * Math.PI / 180.0);
+	}
+
+	public double rad2deg(double rad)
+	{
+		return (rad * 180 / Math.PI);
+	}
 }
