2a09b98043ff8b1abbf2372a7337e4369135b6bf
HyunSangWon <hyunsangwon93@gmail.com>
2020-06-18 13:59:59 +0900

    GPS 수정

--- src/test/java/pro/cntech/inventory/InventoryApplicationTests.java
+++ src/test/java/pro/cntech/inventory/InventoryApplicationTests.java
@@ -1,8 +1,15 @@
 package pro.cntech.inventory;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 import pro.cntech.inventory.service.StatusService;
 
 import java.util.ArrayList;
@@ -12,13 +19,17 @@ import java.util.List;
 class InventoryApplicationTests
 {
   	//http://www.geodatasource.com/developers/ 참고
+	private String API_KEY = "87c48b54b7f2c7fdc3f31063f02a7fa2";
+
 	@Test
-	void contextLoads()
+	void contextLoads() throws Exception
 	{
 		// 킬로미터(Kilo Meter) 단위
-		int distanceKiloMeter =
+		/*int distanceKiloMeter =
 				(int)distance(36.4324528, 127.3944195, 36.343869459442, 127.448909230153, "meter");
-		System.out.println("distance ====> "+distanceKiloMeter);
+		System.out.println("distance ====> "+distanceKiloMeter);*/
+		String gps = convertAddrToGPS("서울특별시 송파구 위례성대로 6");
+		System.out.println("GPS =======> "+gps);
  	}
 
  	public double distance(double gpsLat, double gpsLon, double companyLat, double companyLon, String unit)
@@ -46,4 +57,29 @@ class InventoryApplicationTests
 	{
 		return (rad * 180 / Math.PI);
 	}
+
+	public String convertAddrToGPS(String addr) throws Exception
+	{
+
+		final String API_URL = "https://dapi.kakao.com/v2/local/search/address.json?query="+addr;
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization","KakaoAK "+API_KEY);
+
+		RestTemplate rest = new RestTemplate();
+		ResponseEntity<String> result = rest.exchange(API_URL, HttpMethod.GET, new org.springframework.http.HttpEntity(headers),String.class);
+
+		JSONParser jsonParser = new JSONParser();
+		JSONObject jsonObject = (JSONObject) jsonParser.parse(result.getBody());
+		JSONArray jsonArray = (JSONArray)jsonObject.get("documents");
+
+		JSONObject local = (JSONObject)jsonArray.get(0);
+		JSONObject jsonArray1 = (JSONObject)local.get("address");
+
+		String longitude = jsonArray1.get("x").toString();//경도 127.
+		String latitude = jsonArray1.get("y").toString();//위도 36.
+
+		return longitude+"/"+latitude;
+	}
+
+
 }
